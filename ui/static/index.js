1eaaa858d0a76528d1d0abac9e2f297beff852f8
jackmford <jackmitchellfordyce@gmail.com>
2023-04-04 14:56:20 -0500

    feat: simplified logic of this js function

---
+++ ui/static/index.js
@@ -0,0 +1,37 @@
+function changeTab(film_type, btn_name) {
+  //var tabs = document.getElementsByClassName("tabcontent");
+  //for (i = 0; i < tabs.length; i++) {
+  //  tabs[i].style.display = "none";
+  //}
+
+  document.getElementById(film_type).style.display = "";
+
+  if(film_type == "film") {
+    document.getElementById('iphone').style.display = "none";
+  } else {
+    document.getElementById('film').style.display = "none";
+  }
+
+  //var selected = document.getElementsByClassName("active");
+  //if (selected.length > 0) {
+  //  selected[0].className = "";
+  //}
+  //document.getElementById(btn_name).className = "active";
+}
+
+//document.getElementById('film_button').className = "active";
+document.getElementById('iphone').style.display = "none";
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+const loader = document.querySelector(".loader");
+window.onload = function(){
+  setTimeout(function(){
+      loader.style.opacity = "0";
+      setTimeout(function(){
+          loader.style.display = "none";
+          }, 500);
+      },1500);
+}
